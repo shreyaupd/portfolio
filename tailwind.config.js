2e5cc72de605e0478803c05451a79b21d34fef87
shreyaupd <shreyaupd234@gmail.com>
2024-09-06 19:55:55 +0545

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      screens: {
+        sm: '480px',
+      },
+      fontFamily: {
+        DancingScript: ['Dancing Script', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
